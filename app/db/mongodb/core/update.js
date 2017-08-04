8bfd7dce9dc59cd4def359b9f452b6acc040d068
Utkarsh85 <utkarsh@getfrapp.com>
2017-08-04 22:20:31 +0530

    Changed how core/update and core/updateOne behaves, now will fetch the right changed documents

--- app/db/mongodb/core/update.js
+++ app/db/mongodb/core/update.js
@@ -3,16 +3,23 @@ var queryHelper= require('./utils/queryHelper');
 
 module.exports= function (db,modelName,selector,obj,findProjection) {
 
+	var filter;
+
 	if(!selector)
 			selector={};
 
 	var selectObj= queryHelper._id(selector);
 
-	return db.collection( modelName )
-	.updateMany(selectObj,{'$set':obj})
+	return find(db,modelName,selector,{_id: 1}).toArray()
+	.then(function (matchingIds) {
+		filter= {_id: {$in: matchingIds.map(doc=>doc._id)}};
+
+		return db.collection( modelName )
+		.updateMany(filter,{'$set':obj});
+	})
 	.then(function (docs) {
 		if(docs.result.ok && docs.result.nModified>=1)
-			return find(db,modelName,selector,findProjection).toArray();
+			return find(db,modelName,filter,findProjection).toArray();
 		else
 			return [];
 	})
--- app/db/mongodb/core/updateOne.js
+++ app/db/mongodb/core/updateOne.js
@@ -3,16 +3,27 @@ var queryHelper= require('./utils/queryHelper');
 
 module.exports= function (db,modelName,selector,obj,findProjection) {
 
+	var filter;
+
 	if(!selector)
 			selector={};
 
 	var selectObj= queryHelper._id(selector);
 
-	return db.collection( modelName )
-	.updateOne(selectObj,{'$set':obj})
+	return findOne(db,modelName,selector,{_id: 1})
+	.then(function (matchingId) {
+		if(matchingId)
+		{
+			filter= {_id: {$in: [matchingId._id]}};
+			return db.collection( modelName )
+			.updateOne(filter,{'$set':obj})
+		}
+		else
+			return {result:{ok:1,nModified:0}};
+	})
 	.then(function (docs) {
 		if(docs.result.ok && docs.result.nModified>=1)
-			return findOne(db,modelName,selector,findProjection)
+			return findOne(db,modelName,filter,findProjection);
 		else
 			return null;
 	})
