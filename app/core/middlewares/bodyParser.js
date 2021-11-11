6ad295d21f37cf1388b0f87779929ee1eaed9067
Yashbeer <yashbeer@futwork.com>
2021-11-11 07:14:56 +0000

    Increased payload limit to 2mb

--- app/core/middlewares/bodyParser.js
+++ app/core/middlewares/bodyParser.js
@@ -1,6 +1,6 @@
 var bodyParser = require('body-parser');
 
 module.exports= function (app) {
-	app.use(bodyParser.json()); // for parsing application/json
-	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+	app.use(bodyParser.json({limit: '2mb'})); // for parsing application/json
+	app.use(bodyParser.urlencoded({limit: '2mb', extended: true})); // for parsing application/x-www-form-urlencoded
 }
